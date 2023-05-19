2fa25ab1a6903fde9e3c22fe92307daf40628842
Marc Norris <shaky411@googlemail.com>
2023-05-19 22:56:21 +0100

    updated readme

---
+++ README.md
@@ -0,0 +1,45 @@
+# Simple Weather App
+
+![licence](https://img.shields.io/badge/licence-MIT-yellow.svg)
+
+## Simple weather application
+
+This is a simple to use weather application without all of the clutter that many weather apps display.
+
+This app is built with HTML, JavaScript & styled with TailwindCSS.
+
+<br>
+
+**Table of Contents:**
+
+* [Usage](#usage)
+* [Screenshots](#screenshots)
+* [Credits](#credits)
+* [License](#license)
+
+<br>
+
+# Usage
+
+Visit the application here: https://shaky411.github.io/Current-Weather/
+
+<br>
+
+## Screenshots
+
+![Alt text](assets/sc-0001.png)
+
+![Alt text](assets/sc-0002.png)
+
+<br>
+
+## Credits
+
+N/A
+
+<br>
+
+## License
+
+* _This application has the MIT Licence_
+For more info, please go to the [licence description](https://opensource.org/license/mit/)
--- drivers.js
+++ drivers.js
@@ -1,117 +1,108 @@
-const cityInput = document.getElementById('city-input');
-const searchBtn = document.getElementById('search');
-const weather = document.getElementById('weather-container');
-const title = document.getElementById('cityTitle');
-const wind = document.getElementById('wind');
-const direction = document.getElementById('direction');
-const rain = document.getElementById('rain');
-const temp = document.getElementById('temp');
-const itemContainer = document.getElementById('item-container');
-const dailyData = document.getElementById('daily-data');
-const spanData = document.getElementById('span-data');
-const subTit = document.getElementById('subTitle');
-const pageTitle = document.getElementById('page-title');
-const dailyContainer = document.getElementById('daily-container');
-const loader = document.getElementById('loading');
+const cityInput = document.getElementById("city-input");
+const searchBtn = document.getElementById("search");
+const weather = document.getElementById("weather-container");
+const title = document.getElementById("cityTitle");
+const wind = document.getElementById("wind");
+const direction = document.getElementById("direction");
+const rain = document.getElementById("rain");
+const temp = document.getElementById("temp");
+const itemContainer = document.getElementById("item-container");
+const dailyData = document.getElementById("daily-data");
+const spanData = document.getElementById("span-data");
+const subTit = document.getElementById("subTitle");
+const pageTitle = document.getElementById("page-title");
+const dailyContainer = document.getElementById("daily-container");
+const loader = document.getElementById("loading");
 
-const weatherImage = document.getElementById('w-image');
+const weatherImage = document.getElementById("w-image");
 
 const apiKEY = "6wqm0f4vkilufitxhwxlf06d8t39svnfbhbou4gm";
 
-Object.defineProperty(String.prototype, 'capitalize', {
-    value: function() {
-      return this.charAt(0).toUpperCase() + this.slice(1);
-    },
-    enumerable: false
-  });
-
-  
+Object.defineProperty(String.prototype, "capitalize", {
+  value: function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+  },
+  enumerable: false,
+});
 
 async function fetchData() {
-      
-      const response = await fetch(`https://www.meteosource.com/api/v1/free/point?place_id=${cityInput.value}&sections=current%2C%20daily&language=en&units=auto&key=${apiKEY}`);
-      const data = await response.json();
-      hideLoading()
-      // Handle the data returned from the API
-      const allData = data;
-      console.log(allData)
-      const daily = allData.daily.data
-      console.log(daily)
+  const response = await fetch(
+    `https://www.meteosource.com/api/v1/free/point?place_id=${cityInput.value}&sections=current%2C%20daily&language=en&units=auto&key=${apiKEY}`
+  );
+  const data = await response.json();
+  hideLoading();
+  // Handle the data returned from the API
+  const allData = data;
+  console.log(allData);
+  const daily = allData.daily.data;
+  console.log(daily);
 
-      let dailyCards = "";
+  let dailyCards = "";
 
-      // 7 day summary
-      daily.forEach(element => {
-        let data = element
-        // console.log(data)
-        // console.log(data.all_day);
+  // 7 day summary
+  daily.forEach((element) => {
+    let data = element;
+    // console.log(data)
+    // console.log(data.all_day);
 
-        dailyCards += `<div class="daily-data">
+    dailyCards += `<div class="daily-data">
         <h2 class="day">${data.day}</h2>
         <span id="summary">${data.all_day.weather}</span>
         <div class="temp-data">
-        <span id="min-temp">Min Temp: ${data.all_day.temperature_min}</span>
+        <i class="fa-solid fa-temperature-low text-slate-300"></i><span id="min-temp">Min Temp: ${data.all_day.temperature_min}</span>
         <span id="max-temp">Max Temp: ${data.all_day.temperature_max}</span>
         </div>
         </div>
-        `
-      });
+        `;
+  });
 
-      dailyData.innerHTML = dailyCards;
+  dailyData.innerHTML = dailyCards;
 
-      const conditions = data.current.summary;
-      const temperature = data.current.temperature;
+  const conditions = data.current.summary;
+  const temperature = data.current.temperature;
 
-      console.log(data.current)
+  console.log(data.current);
 
-    const wData = document.getElementById('test');
+  const wData = document.getElementById("test");
 
-    wind.innerHTML = `${data.current.wind.speed} mph`
-    direction.innerHTML = `${data.current.wind.dir}`
-    rain.innerHTML = `${data.current.precipitation.type}`
-    temp.innerHTML = `${temperature}℃`
-    
+  wind.innerHTML = `${data.current.wind.speed} mph`;
+  direction.innerHTML = `${data.current.wind.dir}`;
+  rain.innerHTML = `${data.current.precipitation.type}`;
+  temp.innerHTML = `${temperature}℃`;
 
-    title.innerHTML = cityInput.value.capitalize();
-    wData.innerHTML = `Current conditions: <strong>${conditions}</strong>`;
+  title.innerHTML = cityInput.value.capitalize();
+  wData.innerHTML = `Current conditions: <strong>${conditions}</strong>`;
 
-    cityInput.value = '';
+  cityInput.value = "";
+}
+
+function validateInput() {
+  if (cityInput.value === "") {
+    alert("please enter a city");
+  } else {
+    displayLoading();
+    fetchData();
+    itemContainer.classList.remove("hidden");
+    subTit.classList.remove("hidden");
+    pageTitle.classList.add("hidden");
   }
+}
 
-  function validateInput() {
-    if (cityInput.value === "") {
-        alert("please enter a city");
-    } else {
-      displayLoading();
-        fetchData()
-        itemContainer.classList.remove('hidden')
-        subTit.classList.remove('hidden')
-        pageTitle.classList.add('hidden');
-    }
-  }
+function displayLoading() {
+  loader.classList.add("display");
 
-  function displayLoading() {
-    loader.classList.add("display");
-
-    setTimeout(() => {
-      loader.classList.remove("display");
-    }, 5000)
-  }
-
-  function hideLoading() {
+  setTimeout(() => {
     loader.classList.remove("display");
-  }
+  }, 5000);
+}
 
-  function showSummary() {
-    dailyContainer.classList.toggle('hidden');
-  }
+function hideLoading() {
+  loader.classList.remove("display");
+}
 
-  searchBtn.addEventListener('click', validateInput);
-  subTit.addEventListener('click', showSummary );
+function showSummary() {
+  dailyContainer.classList.toggle("hidden");
+}
 
-  
-
-
-//   `
-//     <h1>The current conditions are <strong>${conditions}</strong></h1>
-//     `
+searchBtn.addEventListener("click", validateInput);
+subTit.addEventListener("click", showSummary);
