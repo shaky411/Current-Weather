1984651058790cbbacd3764aaa385e8b5eb97900
Marc Norris <shaky411@googlemail.com>
2023-06-06 12:43:34 +0100

    updated place id function

--- drivers.js
+++ drivers.js
@@ -21,7 +21,7 @@ const mainLogo = document.getElementById("mainLogo");
 const tagline = document.getElementById("tagline");
 const high = document.getElementById("dayHigh");
 const low = document.getElementById("dayLow");
-const highLowContainer = document.getElementById('day-high-low');
+const highLowContainer = document.getElementById("day-high-low");
 
 const apiKEY = "6wqm0f4vkilufitxhwxlf06d8t39svnfbhbou4gm";
 
@@ -32,103 +32,177 @@ Object.defineProperty(String.prototype, "capitalize", {
   enumerable: false,
 });
 
+// function getUserLocation() {
+
+//   function locationSuccess(position) {
+//     console.log("success");
+//     let coords = position.coords;
+//     getLocationData(coords);
+//   }
+
+//   function locationError() {
+//     console.log("error")
+//   }
+//   navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+// }
+
+// function getLocationData(coords) {
+
+//   var URL = 'http://example.com/?lat=' + coords.latitude + '&long=' + coords.longitude;
+//   console.log(URL);
+//   console.log(coords.latitude)
+//   console.log(coords.longitude)
+// }
+
+// function app() {
+//   displayLoading();
+//   getUserLocation();
+// }
+
+// window.onload = app();
+
+// // data on load by location
+// async function getLocationData(coords) {
+
+//   const response = await fetch(
+//     `https://www.meteosource.com/api/v1/free/nearest_place?lat=${coords.latitude}&lon=${coords.longitude}&key=${apiKEY}`
+//   );
+
+//   const location = await response.json();
+//   let locationId = location.place_id;
+//   console.log(locationId);
+
+//   const locationResponse = await fetch(
+//     `https://www.meteosource.com/api/v1/free/point?place_id=${locationId}&sections=all%2C%20daily&language=en&units=metric&key=${apiKEY}`
+//   )
+
+//   const localeData = locationResponse.json();
+//   hideLoading();
+//   mainLogo.classList.add("hidden");
+//   const allLocaleData = localeData;
+//   const eachDay = allLocaleData;
+//   console.log(eachDay)
+
+// wind.innerHTML = `${data.current.wind.speed} mph`;
+// direction.innerHTML = `${data.current.wind.dir}`;
+// rain.innerHTML = `${data.current.precipitation.type}`;
+// temp.innerHTML = `${temperature}℃`;
+// titleTemp.innerHTML = `${temperature}℃`;
+// High-Low temperature for the current day
+// high.innerHTML = `H:<strong>${daily[0].all_day.temperature_max}℃</strong>`;
+// low.innerHTML = `L:<strong>${daily[0].all_day.temperature_min}℃</strong>`;
+
+// }
+
+// getLocationData()
+
 async function fetchData() {
   try {
+    // find place id
     const response = await fetch(
-      `https://www.meteosource.com/api/v1/free/point?place_id=${cityInput.value.trim()}&sections=all%2C%20daily&language=en&units=metric&key=${apiKEY}`
+      `https://www.meteosource.com/api/v1/free/find_places?text=${cityInput.value.trim()}&key=${apiKEY}`
     );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
 
-    const data = await response.json();
-    hideLoading();
-    mainLogo.classList.add("hidden");
+    const placeData = await response.json();
+    let placeId = placeData[0].place_id;
+  
+    // use place id to search for city
+  const placeResponse = await fetch(
+    `https://www.meteosource.com/api/v1/free/point?place_id=${placeId}&sections=all%2C%20daily&language=en&units=metric&key=${apiKEY}`
+  );
 
-    // Handle the data returned from the API
-    const allData = data;
-    console.log(allData);
-    const daily = allData.daily.data;
-    console.log(daily);
+  const data = await placeResponse.json();
+  hideLoading();
+  mainLogo.classList.add("hidden");
 
-    // Rest of the code...
-    let dailyCards = "";
-    let dailyModal = "";
+  // Handle the data returned from the API
+  const allData = data;
+  console.log(allData);
+  const daily = allData.daily.data;
+  console.log(daily);
 
-    // High-Low temperature for the current day
-    high.innerHTML = `H:<strong>${daily[0].all_day.temperature_max}℃</strong>`;
-    low.innerHTML = `L:<strong>${daily[0].all_day.temperature_min}℃</strong>`;
+  // Rest of the code...
+  let dailyCards = "";
+  let dailyModal = "";
 
-    // 7 day summary - get the data for each day
-    daily.forEach((element) => {
-      let data = element;
-      // console.log(data)
-      // console.log(data.all_day);
+  // High-Low temperature for the current day
+  high.innerHTML = `H:<strong>${daily[0].all_day.temperature_max}℃</strong>`;
+  low.innerHTML = `L:<strong>${daily[0].all_day.temperature_min}℃</strong>`;
 
-      // Get the icon code and add it to the src string from the image assets
-      let icon = data.icon;
-      let iconSource = "./assets/weather-icons/" + icon + ".png";
+  // 7 day summary - get the data for each day
+  daily.forEach((element) => {
+    let data = element;
+    // console.log(data)
+    // console.log(data.all_day);
 
-      // re-format day data
-      let options = { weekday: "long", month: "long", day: "numeric" };
-      let today = new Date(data.day);
-      let correctData = today.toLocaleString("en-us", options);
-      console.log(correctData);
+    // Get the icon code and add it to the src string from the image assets
+    let icon = data.icon;
+    let iconSource = "./assets/weather-icons/" + icon + ".png";
 
-      dailyCards += `<div class="daily-data">
-        <div class="card-date">
-        <h2 class="day">${correctData}</h2>
-        </div>
-        <div class="icon"><img src="${iconSource}" alt="weather icon"</img></div>
-        <span id="summary">${data.all_day.weather}</span>
-        <div class="temp-data">
-        <i class="fa-solid fa-temperature-low text-slate-300"></i><span id="min-temp">Min Temp: <strong>${data.all_day.temperature_min}</strong>℃</span>
-        <span id="max-temp">Max Temp: <strong>${data.all_day.temperature_max}</strong>℃</span>
-        </div>
-        <div class="card-wind">
-        <i class="fa-solid fa-wind mt-2"></i>
-        <span><strong>${data.all_day.wind.speed}</strong>mph</span>
-        </div>
-        <div>
-        <i class="fa-solid fa-umbrella"></i>
-        <span><strong>${data.all_day.precipitation.type}</strong></span>
-        </div>
-        </div>
-        `;
+    // re-format day data
+    let options = { weekday: "long", month: "long", day: "numeric" };
+    let today = new Date(data.day);
+    let correctData = today.toLocaleString("en-us", options);
+    console.log(correctData);
 
-    });
+    dailyCards += `<div class="daily-data">
+      <div class="card-date">
+      <h2 class="day">${correctData}</h2>
+      </div>
+      <div class="icon"><img src="${iconSource}" alt="weather icon"</img></div>
+      <span id="summary">${data.all_day.weather}</span>
+      <div class="temp-data">
+      <i class="fa-solid fa-temperature-low text-slate-300"></i><span id="min-temp">Min Temp: <strong>${data.all_day.temperature_min}</strong>℃</span>
+      <span id="max-temp">Max Temp: <strong>${data.all_day.temperature_max}</strong>℃</span>
+      </div>
+      <div class="card-wind">
+      <i class="fa-solid fa-wind mt-2"></i>
+      <span><strong>${data.all_day.wind.speed}</strong>mph</span>
+      </div>
+      <div>
+      <i class="fa-solid fa-umbrella"></i>
+      <span><strong>${data.all_day.precipitation.type}</strong></span>
+      </div>
+      </div>
+      `;
+  });
 
-    let currentIcon = data.current.icon_num;
-    let currentIconSource =
-      "./assets/weather-icons-big/" + currentIcon + ".png";
-    titleIcon.innerHTML = `<img src="${currentIconSource}" alt="weather icon"</img>`;
+  let currentIcon = data.current.icon_num;
+  let currentIconSource = "./assets/weather-icons-big/" + currentIcon + ".png";
+  titleIcon.innerHTML = `<img src="${currentIconSource}" alt="weather icon"</img>`;
 
-    dailyData.innerHTML = dailyCards;
+  dailyData.innerHTML = dailyCards;
 
-    const conditions = data.current.summary;
-    const temperature = data.current.temperature;
+  const conditions = data.current.summary;
+  const temperature = data.current.temperature;
 
-    console.log(data.current);
+  console.log(data.current);
 
-    const wData = document.getElementById("test");
+  const wData = document.getElementById("test");
 
-    wind.innerHTML = `${data.current.wind.speed} mph`;
-    direction.innerHTML = `${data.current.wind.dir}`;
-    rain.innerHTML = `${data.current.precipitation.type}`;
-    temp.innerHTML = `${temperature}℃`;
-    titleTemp.innerHTML = `${temperature}℃`;
+  wind.innerHTML = `${data.current.wind.speed} mph`;
+  direction.innerHTML = `${data.current.wind.dir}`;
+  rain.innerHTML = `${data.current.precipitation.type}`;
+  temp.innerHTML = `${temperature}℃`;
+  titleTemp.innerHTML = `${temperature}℃`;
 
-    title.innerHTML = cityInput.value.capitalize();
-    wData.innerHTML = `Current conditions: <strong>${conditions}</strong>`;
+  title.innerHTML = cityInput.value.capitalize();
+  wData.innerHTML = `Current conditions: <strong>${conditions}</strong>`;
 
-    cityInput.value = "";
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    showErrorModal();
-    // You can handle the error here, e.g., display an error message to the user.
-  }
+  cityInput.value = "";
+
+} catch (error) {
+  console.error("Error fetching data:", error);
+  alert("Error! Please check name and try again.");
+  // You can handle the error here, e.g., display an error message to the user.
 }
+
+}
+
 
 // Validate input, start loader and fetch data
 function validateInput() {
@@ -137,10 +211,11 @@ function validateInput() {
   } else {
     displayLoading();
     fetchData();
+    // findPlace();
 
     itemContainer.classList.remove("hidden");
     subTit.classList.remove("hidden");
-    highLowContainer.classList.remove('hidden');
+    highLowContainer.classList.remove("hidden");
     pageTitle.classList.add("hidden");
     tagline.classList.add("hidden");
   }
@@ -167,78 +242,73 @@ function showSummary() {
   dailyContainer.classList.toggle("hidden");
 }
 
-const modalBg = document.getElementById('modal-bg');
-const modalContent = document.getElementById('modal-content');
-const closeModal = document.getElementById('close-modal');
+const modalBg = document.getElementById("modal-bg");
+const modalContent = document.getElementById("modal-content");
+const closeModal = document.getElementById("close-modal");
 
 function showModal() {
-  modalBg.classList.remove('hidden')
-  modalContent.classList.remove('hidden')
+  modalBg.classList.remove("hidden");
+  modalContent.classList.remove("hidden");
 }
 
 function hideModal() {
-  modalBg.classList.add('hidden')
-  modalContent.classList.add('hidden')
+  modalBg.classList.add("hidden");
+  modalContent.classList.add("hidden");
 }
 
-const errorModalBg = document.getElementById('error-modal-bg');
-const errorModalContent = document.getElementById('error-modal-content');
-const closeErrorModal = document.getElementById('close-error-modal');
-const infoModalBtn = document.getElementById('info-modal');
-const infoModalBg = document.getElementById('info-modal-bg');
-const infoModalContent = document.getElementById('info-modal-content');
+const errorModalBg = document.getElementById("error-modal-bg");
+const errorModalContent = document.getElementById("error-modal-content");
+const closeErrorModal = document.getElementById("close-error-modal");
+const infoModalBtn = document.getElementById("info-modal");
+const infoModalBg = document.getElementById("info-modal-bg");
+const infoModalContent = document.getElementById("info-modal-content");
 
 function showErrorModal() {
-  errorModalBg.classList.remove('hidden')
-  errorModalContent.classList.remove('hidden')
+  errorModalBg.classList.remove("hidden");
+  errorModalContent.classList.remove("hidden");
 }
 
 function hideErrorModal() {
-  errorModalBg.classList.add('hidden')
-  errorModalContent.classList.add('hidden')
+  errorModalBg.classList.add("hidden");
+  errorModalContent.classList.add("hidden");
 }
 
 // Event listeners
 searchBtn.addEventListener("click", validateInput);
 subTit.addEventListener("click", showSummary);
-closeModal.addEventListener('click', hideModal);
-closeErrorModal.addEventListener('click', hideErrorModal);
+closeModal.addEventListener("click", hideModal);
+closeErrorModal.addEventListener("click", hideErrorModal);
 
+const modal = document.querySelector(".main-modal");
+const closeButton = document.querySelectorAll(".modal-close");
 
+function modalClose() {
+  modal.classList.remove("fadeIn");
+  modal.classList.add("fadeOut");
+  setTimeout(() => {
+    modal.style.display = "none";
+  }, 500);
+}
 
+function openModal() {
+  modal.classList.remove("fadeOut");
+  modal.classList.add("fadeIn");
+  modal.style.display = "flex";
+}
 
-const modal = document.querySelector('.main-modal');
-const closeButton = document.querySelectorAll('.modal-close');
+for (let i = 0; i < closeButton.length; i++) {
+  const elements = closeButton[i];
 
-    function modalClose() {
-        modal.classList.remove('fadeIn');
-        modal.classList.add('fadeOut');
-        setTimeout(() => {
-            modal.style.display = 'none';
-        }, 500);
-    }
+  elements.onclick = (e) => modalClose();
 
-    function openModal() {
-        modal.classList.remove('fadeOut');
-        modal.classList.add('fadeIn');
-        modal.style.display = 'flex';
-    }
+  modal.style.display = "none";
 
-    for (let i = 0; i < closeButton.length; i++) {
+  window.onclick = function (event) {
+    if (event.target == modal) modalClose();
+  };
+}
 
-        const elements = closeButton[i];
-
-        elements.onclick = (e) => modalClose();
-
-        modal.style.display = 'none';
-
-        window.onclick = function (event) {
-            if (event.target == modal) modalClose();
-        }
-    }
-
-
-    // About Modal
+// About Modal
 
 const aboutModal = document.querySelector(".about-modal");
 const aboutCloseButton = document.querySelectorAll(".about-modal-close");
@@ -269,6 +339,6 @@ for (let i = 0; i < aboutCloseButton.length; i++) {
   };
 }
 
-const aboutBtn = document.getElementById('about');
+const aboutBtn = document.getElementById("about");
 
-aboutBtn.addEventListener('click', aboutOpenModal);
+aboutBtn.addEventListener("click", aboutOpenModal);
